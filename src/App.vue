8a5f1863b3bb0a937ea99bb5e31872afcf4e49e1
BassXiaoSen <314121378@qq.com>
2020-12-17 23:59:16 +0800

    add element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
   <div id="app">
     <h1>Stub</h1>
+    <p>{{a}}</p>
     <!-- 根路由 -->
     <router-view/>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
 
+@Component
+export default class App extends Vue {
+  a = 1
+  b = '2'
+  c = {
+    a: 1,
+    b: '2'
+  }
+
+  test () {
+    console.log(this.a)
+  }
+}
+</script>
 <style lang="scss" scoped>
 </style>
