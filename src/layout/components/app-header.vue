e586f30afe1c3e9d7c66c77641dc45c8b8acf141
BassXiaoSen <314121378@qq.com>
2020-12-20 15:04:18 +0800

    处理用户退出

--- src/layout/components/app-header.vue
+++ src/layout/components/app-header.vue
@@ -14,7 +14,9 @@
       </span>
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item>{{userInfo.userName}}</el-dropdown-item>
-        <el-dropdown-item divided>退出</el-dropdown-item>
+        <el-dropdown-item divided
+          @click.native="handleLogout"
+        >退出</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
   </div>
@@ -38,6 +40,20 @@ export default Vue.extend({
     async loadUserInfo () {
       const { data } = await getUserInfo()
       this.userInfo = data.content
+    },
+    handleLogout () {
+      // 清除登录状态
+      // 跳转到登页面
+      this.$confirm('确认退出吗？', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$store.commit('setUser', null)
+        this.$router.push({ name: 'login' })
+      }).catch(() => {
+        this.$message('取消退出')
+      })
     }
   }
 })
